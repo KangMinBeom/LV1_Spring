25f1647ffcd0c59df7a7aa0b1f9e0710d5570ca3
kangminbeom <alsqja6945@naver.com>
2023-11-08 09:53:47 +0900

    first commit

---
+++ src/main/java/com/sparta/lv1_spring/Lv1SpringApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.lv1_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lv1SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lv1SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/lv1_spring/Lv1SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.lv1_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lv1SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
